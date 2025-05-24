634990f445f51fcf416a9730e4e17e568859f62e
Fahad Ahmad <fahadahmad48224@gmail.com>
2025-05-25 01:47:42 +0500

    add some changes in about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
   return (
     <section
       id="aboutSection"
-      className="w-full md:h-[600px] h-auto bg-white px-5 sm:px-20 relative pb-16"
+      className="w-full h-auto bg-white px-5 sm:px-20 relative pb-16"
     >
       <span className="bg-dot-pattern bg-[length:40px_40px] w-full sm:w-[60%] h-[25%] absolute top-0 left-[50%] translate-x-[-50%]" />
       <GsapAnimation>
@@ -16,10 +16,10 @@ function About() {
         <span className="text-black Bricolage z-10">am I?</span>
       </h2>
       </GsapAnimation>
-      <div className="md:flex md:flex-row justify-between items-center flex flex-col-reverse gap-x-10 mt-12">
-        <div className="md:w-[50%] w-full md:text-start text-center">
+      <div className="lg:flex lg:flex-row justify-center items-center flex flex-col-reverse gap-x-20 mt-12">
+        <div className="md:w-[50%] w-full lg:text-start text-center">
           <GsapAnimation>
-          <h3 className="text-3xl font-bold Bricolage mt-5 md:mt-0">
+          <h3 className="text-3xl font-bold Bricolage mt-5 lg:mt-0">
             Hi 👋, I am
             <span className="text-blue-500 font-bold Bricolage pl-2">
               Fahad Ahmad
@@ -37,7 +37,7 @@ function About() {
             beautiful and functional.
           </p>
           </GsapAnimation>
-          <div className="flex justify-center md:justify-start gap-x-5 mt-5">
+          <div className="flex justify-center lg:justify-start gap-x-5 mt-5">
             <a target="_blank" href="https://github.com/fahad-ahmad403">
               <Github className="text-blue-500 text-xl hoverable:hover:text-blue-600 transition duration-300" />
             </a>
@@ -45,7 +45,7 @@ function About() {
               <Linkedin className="text-blue-500 text-xl hoverable:hover:text-blue-600 transition duration-300" />
             </a>
           </div>
-          <ArrowButton className="text-blue-500 bg-black hoverable:hover:text-black" spanclassName="bg-blue-600" aclassName="flex justify-center md:justify-start" />
+          <ArrowButton className="text-blue-500 bg-black hoverable:hover:text-black" spanclassName="bg-blue-600" aclassName="flex justify-center lg:justify-start" />
         </div>
         <img className="w-[300px] z-10" loading="lazy" src={Image} alt="Profile picture." />
       </div>
